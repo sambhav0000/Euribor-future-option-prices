f742dd7dfab292878df681902b9653b3166dfefd
sambhav0000 <134165425+sambhav0000@users.noreply.github.com>
2025-11-18 05:06:54 +0530

    Add LinkedIn profile link to sidebar

--- app.py
+++ app.py
@@ -313,6 +313,9 @@ def main():
     # Sidebar for inputs
     with st.sidebar:
         st.header("⚙️ Model Parameters")
+        st.write("`Created by:`")
+        linkedin_url = "https://www.linkedin.com/in/kumar-sambhav-b67142225/"
+        st.markdown(f'<a href="{linkedin_url}" target="_blank" style="text-decoration: none; color: inherit;"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="25" height="25" style="vertical-align: middle; margin-right: 10px;">`Sambhav`</a>', unsafe_allow_html=True)
         
         st.subheader("Market Data")
         future_price = st.number_input("Future Price (%)", 
